d9e8d52e13b4522d988e78b25d49e1d266423bf8
Monika Swidzinska <monikaswidzinska@Monikas-MacBook-Pro.local>
2020-08-18 09:47:00 +0200

    App component:

--- testing-assignment/src/app/app.component.spec.ts
+++ testing-assignment/src/app/app.component.spec.ts
@@ -20,4 +20,17 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
+  it(`should have as title 'testing-assignment'`,() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('testing-assignment');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('testing-assignment app is running!')
+  });
+
 });
