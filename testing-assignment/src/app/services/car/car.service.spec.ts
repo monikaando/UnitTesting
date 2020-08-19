b0f0282a1ca36cf8f2d17832cce326c204f760ad
Monika Swidzinska <monikaswidzinska@Monikas-MacBook-Pro.local>
2020-08-19 15:21:17 +0200

    car service -done

---
+++ testing-assignment/src/app/services/car/car.service.spec.ts
@@ -0,0 +1,18 @@
+import {CarService} from './car.service';
+import {async, TestBed} from '@angular/core/testing';
+
+describe('CarService', () => {
+  let carService: any;
+  carService = new CarService();
+  const newCar = carService.createCar('Maserati', 'Quattroporte', 2018, 4);
+  it('should create and return a new car object', () => {
+    expect(newCar).toEqual(jasmine.objectContaining({
+      make: 'Maserati',
+      model: 'Quattroporte',
+      year: 2018,
+      numWheels: 4
+    }));
+  });
+  it(`should get a car object and return: 'A Maserati Quattroporte from 2018 is a fine car!'`, () => {
+    expect(carService.getCar(newCar)).toEqual('A Maserati Quattroporte from 2018 is a fine car!');
+  });
